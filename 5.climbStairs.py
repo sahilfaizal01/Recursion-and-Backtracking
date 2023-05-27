92afbac3075b3a811197409b85835a8d433fc1ba
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-05-27 04:19:18 -0300

    Update 5.climbStairs.py

--- 5.climbStairs.py
+++ 5.climbStairs.py
@@ -1,6 +1,19 @@
+# Method - 1
 def climbStairs(n):
     if n == 1 or n == 2:
         return n
     return climbStairs(n-1) + climbStairs(n-2)
 
 print(climbStairs(5))
+
+# Method - 2 (More time efficient)
+def climb(n):
+    memo = {}
+    memo[1] = 1
+    memo[2] = 2
+    if n in memo:
+        return memo[n]
+    else:
+        memo[n] = climb(n-1) + climb(n-2)
+        return memo[n]
+print(climb(5))
