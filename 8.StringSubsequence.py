94fdec1450ddcdf42bb26f4296fc03acb469af30
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-06-16 04:50:25 -0300

    Create 8.StringSubsequence.py

---
+++ 8.StringSubsequence.py
@@ -0,0 +1,19 @@
+ans = []
+def PowerSet(s,temp,index):
+    if(len(s)==index):
+        print(temp)
+        ans.append(temp)
+        return
+    
+    # take 
+    PowerSet(s,temp+s[index],index+1)
+    # not take
+    PowerSet(s,temp,index+1)
+    
+    return
+
+s = 'abc'
+temp = ''
+index = 0
+PowerSet(s,temp,index)
+print(ans)
